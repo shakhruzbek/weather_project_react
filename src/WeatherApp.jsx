578006e664776bf3142075fe1b82d416b8129002
shokhruzbek <ashokhruzbek@gmail.com>
2025-04-02 11:43:56 +0500

    i work in console

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import WeatherDetails from './Components/WeatherDetails'
 
 function WeatherApp() {
-  return (
-    <div>WeatherApp</div>
-  )
+    const [weather, setWeather] = useState(null)
+
+    useEffect(() => {
+        axios.get('https://api.weatherapi.com/v1/forecast.json?q=Tashkent&days=3&key=e0c4fcc2ca0449578ae35836241312')
+            .then(response => {
+                setWeather(response.data);
+            })
+    })
+    return (
+        <div>
+            <WeatherDetails location={weather?.location}
+                current={weather?.forecast?.forecastday?.[0].day}
+            />
+
+        </div>
+    )
 }
 
 export default WeatherApp
