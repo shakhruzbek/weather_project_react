5e043972e1e9cb2d536db02d95fa676cb56db680
shokhruzbek <ashokhruzbek@gmail.com>
2025-04-02 11:05:45 +0500

    create weather app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import WeatherApp from './WeatherApp'
 
 function App() {
   return (
-    <div>App</div>
+    <WeatherApp/>
   )
 }
 
---
+++ src/WeatherApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WeatherApp() {
+  return (
+    <div>WeatherApp</div>
+  )
+}
+
+export default WeatherApp
