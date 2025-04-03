14c5b43e96504e9ca7fbea8a921a5126496b0b51
shokhruzbek <ashokhruzbek@gmail.com>
2025-04-03 10:15:49 +0500

    some updates

--- src/Components/WeatherDetails.jsx
+++ src/Components/WeatherDetails.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-function WeatherDetails() {
+function WeatherDetails( {location ,current}) {
   return (
-    <div>WeatherDetails</div>
+    <div className='weatherDetail'>
+      <p>{location?.name}</p>
+      <p>{location?.country}</p>
+      <p>{current?.avgtemp_c} °C</p>
+      <img src={current?.condition.icon} alt="icon" />
+      <p>{current?.condition.text}</p>
+    </div>
   )
 }
 
--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,22 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import WeatherDetails from './Components/WeatherDetails'
+import "./WeatherApp.css"
 
-function WeatherApp() {
+
+const WeatherApp = () => {
     const [weather, setWeather] = useState(null)
+    const [isLoading, setLoading] = useState(false)
 
     useEffect(() => {
-        axios.get('https://api.weatherapi.com/v1/forecast.json?q=Tashkent&days=3&key=e0c4fcc2ca0449578ae35836241312')
-            .then(response => {
-                setWeather(response.data);
-            })
-    })
-    return (
-        <div>
-            <WeatherDetails location={weather?.location}
-                current={weather?.forecast?.forecastday?.[0].day}
-            />
+        setLoading(true)
+        const fetchWeather = async () => {
+            setLoading(true)
+            try {
+                const response = await axios.get(
+                    'https://api.weatherapi.com/v1/forecast.json?q=Ottawa&days=3&key=ea6329817e554d2ebc860547250204'
+                )
+                setWeather(response.data)
+            } catch (error) {
+                console.log('Error fetching weather:', error)
+            } finally {
+                setLoading(false)
+            }
+        }
 
+        fetchWeather()
+    }, [])
+
+    return (
+        <div className='container'>
+            {isLoading && <h1 className='load'>Loading...</h1>}
+            {(weather && !isLoading) && (
+                <WeatherDetails
+                    location={weather?.location}
+                    current={weather?.forecast.forecastday[0].day}
+                />
+            )}
         </div>
     )
 }
